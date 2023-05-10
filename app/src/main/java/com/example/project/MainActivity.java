e77de1a5e2b4854402f7b5ea853bd03c4dc21411
E. IGuz <a22edvbu@student.his.se>
2023-05-10 11:35:49 +0200

    Activity Navigation - Lade till knappen buttonGoSecond i MainActivity.java som öppnar SecondActivity.java vid tryck. - buttonSend i SecondActivity.java öppnar MainActivity.java vid tryck.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        Button buttonMain = findViewById(R.id.buttonGoSecond);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent screenIntent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(screenIntent);
+            }
+        });
+    }
 }
