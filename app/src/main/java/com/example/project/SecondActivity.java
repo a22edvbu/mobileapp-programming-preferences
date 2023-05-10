e01550b57a087470367ad18af8a8eb4539979dcc
E. IGuz <a22edvbu@student.his.se>
2023-05-10 12:25:58 +0200

    Shared Preference part 2 - Började implementera överföranded av datan. - Data från editTextAddName läggs till i strängen sharedPrefString.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -3,16 +3,26 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private SharedPreferences sharePref;
+    private SharedPreferences.Editor sharePrefEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        sharePref = getSharedPreferences("sharedPrefString", MODE_PRIVATE);
+        sharePrefEditor = sharePref.edit();
+        EditText addName = findViewById(R.id.editTextAddName);
+        addName.setText(sharePref.getString("sharedPrefString", "Du måste skriva in ditt namn för att det ska visas här."));
 
         Button buttonSecond = findViewById(R.id.buttonSend);
         buttonSecond.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +32,10 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(screenIntent);
             }
         });
+    }
+    public void storedPrefData() {
+        EditText editText = findViewById(R.id.editTextAddName);
+        sharePrefEditor.putString("sharedPrefString", editText.getText().toString());
+        sharePrefEditor.apply();
     }
 }
